00be21bf8cd327f140bfb9a01ebf44c870a0fae2
emelieribring <emelieribring@hotmail.com>
2024-01-31 12:50:43 +0100

    input-component

--- src/Components/SectionFour/SectionFour.jsx
+++ src/Components/SectionFour/SectionFour.jsx
@@ -3,8 +3,28 @@ import './sectionFour.css';
 
 export default function SectionFour() {
   return (
-    <div className='section-four'>
-        
+    <div id='kursStart' className='section-four'>
+        <div className='section-four-title'>
+            <p>Här kan du svara på några frågor om ai och reflektera över hur du använder eller kan använda ai i din vardag/arbetsdag.</p>
+        </div>
+        <div className="section-four-top">
+            <div className="question">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis non, voluptas illo eius rerum id, voluptatem neque veniam ea ratione error dolore! Sapiente iusto atque neque qui est incidunt blanditiis?</p>
+            </div>
+            <textarea className='inputReflection' type="text" placeholder='Write your reflection here'/>
+        </div>
+        <div className="section-four-middle">
+            <div className="question">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis non, voluptas illo eius rerum id, voluptatem neque veniam ea ratione error dolore! Sapiente iusto atque neque qui est incidunt blanditiis?</p>
+            </div>
+            <textarea className='inputReflection' type="text" placeholder='Write your reflection here'/>
+        </div>
+        <div className="section-four-bottom">
+            <div className="question">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis non, voluptas illo eius rerum id, voluptatem neque veniam ea ratione error dolore! Sapiente iusto atque neque qui est incidunt blanditiis?</p>
+            </div>
+            <textarea className='inputReflection' type="text" placeholder='Write your reflection here'/>
+        </div>
     </div>
   )
 }
