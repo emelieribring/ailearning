6b95437233b8e29c23d5d256c9cfb3457d6e7b68
emelieribring <emelieribring@hotmail.com>
2024-02-02 13:44:16 +0100

    text

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,9 +4,7 @@ import './footer.css';
 export default function Footer() {
   return (
     <div className='footer'>
-        <div className='footer-text-div'>
         <p className='footer-text'>Ai GUIDE</p>
-        </div>
     </div>
   )
 }
