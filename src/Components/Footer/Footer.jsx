15e05de6aee6f74c675bce7b75c30dd26d4a14ac
emelieribring <emelieribring@hotmail.com>
2024-02-02 13:45:30 +0100

    text

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,6 +4,16 @@ import './footer.css';
 export default function Footer() {
   return (
     <div className='footer'>
+        <div className="dot"></div> 
+        <p className='made'>made by </p>
+        <p id='atomcollab'>atom collab</p>
+        <p id='o'>o</p>
+        <p id='r'>r</p>
+        <p id='a'>a</p>
+        <p id='t'>t</p>
+        <p id='i'>i</p>
+        <p id='o2'>o</p>
+        <p id='n'>n</p> 
         <p className='footer-text'>Ai GUIDE</p>
     </div>
   )
